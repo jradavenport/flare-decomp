85b32bf1bc11d5daf8fe73f78579d1127fbf18a2
James Davenport <jradavenport@gmail.com>
2016-06-17 09:40:30 -0700

    detrend each flare, prep for fitting

--- flare-decomp.py
+++ flare-decomp.py
@@ -12,6 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import aflare
 import emcee
+import pandas as pd
 
 
 def QPP(t, p):
@@ -53,3 +54,96 @@ def D1P(t, p):
 
     return d1 + p1
 
+
+def D_lnlike(p, x, y, yerr):
+    model = aflare.aflare(x, p)
+    return -0.5 * np.sum(((y - model)/yerr)**2)
+
+
+def Danalysis():
+    '''
+    First exploration: use MCMC with the Davenport et al. (2014) style flare decomposition.
+
+    '''
+
+    # look @ the GJ 1243 flare data...
+    datadir = '/Users/james/Dropbox/research_projects/GJ1243-Flares/data/'
+    datafile = 'gj1243_master_slc.dat'
+    df = pd.read_table(datadir + datafile, delim_whitespace=True, comment='#', header=None,
+                       names=['time','flux','err','dflux'])
+
+    medflux = np.nanmedian(df['dflux'])
+
+    # 5 large complex flares that I picked out by hand in 2014, representing different morphologies
+    ftimes = [[916.54, 916.66],
+              [581.955, 582.025],
+              [549.75, 549.895],
+              [542.972, 543.015],
+              [555.80, 555.865]]
+
+    # my by-eye estimate of the number of components to fit
+    Nfl = [8, 7, 9, 4, 7]
+
+    '''
+    Plan:
+    0. get data only within timespan
+    1. subtract local linear continuum off, put in % units
+    2. make seed for N flares
+    3. run thru MCMC
+    4. draw 100 likely flares over the data
+
+    Can put hard limits on parameters (e.g. fwhm) using a ln-likelihood function that
+    blows up if a limit is hit.
+
+    '''
+
+    k = 2 # start with the most complex flare
+
+    x = np.where((df['time'] >= ftimes[k][0]) &
+                 (df['time'] <= ftimes[k][1]))[0]
+
+    t_fl = df['time'].values[x]
+    f_fl = df['flux'].values[x]
+    e_fl = df['err'].values[x]
+
+    e_fl = e_fl / medflux
+
+    # fit a line thru the flare, remove, normalize
+    c0 = np.median(f_fl[0:5])
+    c1 = np.median(f_fl[-5:])
+    t0 = np.median(t_fl[0:5])
+    t1 = np.median(t_fl[-5:])
+    slope = (c1-c0) / (t1-t0)
+    inter = c0 - slope * t0
+    fline = t_fl * slope + inter
+
+    f_fl = (f_fl - fline) / medflux
+
+    # test plot
+    plt.figure()
+    plt.errorbar(t_fl, f_fl, yerr=e_fl)
+    plt.show()
+
+
+    time_0 = np.nanmin(t_fl)
+    fwhm_0 = 2. / 60. / 24. # 2 min
+    ampl_0 = 0.01 # 1% guess
+
+    p0 = np.tile([time_0, fwhm_0, ampl_0], Nfl[k])
+    # for l in range(Nfl[k]):
+    #     p0
+
+
+    nsteps = 1000
+    nwalkers = 50
+    ndim = len(p0)
+
+    # sampler = emcee.EnsembleSampler(nwalkers, ndim, D_lnlike,
+    #                                 args=[t_fl, f_fl, e_fl] )
+    #
+    # sampler.run_mcmc(p0, nsteps)
+
+
+if __name__ == "__main__":
+    # import sys
+    Danalysis()
